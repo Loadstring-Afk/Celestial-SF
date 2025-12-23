f010b4869fb08689cc8d46ff1da3d58faff519a4
Loadstring-Afk <elvismateopedro@gmail.com>
2025-12-23 12:21:34 -0600

    Create config/security-profiles.js

---
+++ config/security-profiles.js
@@ -0,0 +1,62 @@
+{
+  "basic": {
+    "stringEncryption": true,
+    "variableRenaming": true,
+    "controlFlowObfuscation": false,
+    "deadCodeInjection": false,
+    "vmObfuscation": false,
+    "antiDebug": false,
+    "antiTampering": false,
+    "obfuscationLevel": 2
+  },
+  "standard": {
+    "stringEncryption": true,
+    "variableRenaming": true,
+    "controlFlowObfuscation": true,
+    "deadCodeInjection": true,
+    "vmObfuscation": false,
+    "antiDebug": true,
+    "antiTampering": false,
+    "obfuscationLevel": 5
+  },
+  "professional": {
+    "stringEncryption": true,
+    "variableRenaming": true,
+    "controlFlowObfuscation": true,
+    "deadCodeInjection": true,
+    "vmObfuscation": true,
+    "antiDebug": true,
+    "antiTampering": true,
+    "integrityChecks": true,
+    "obfuscationLevel": 7
+  },
+  "enterprise": {
+    "stringEncryption": true,
+    "variableRenaming": true,
+    "controlFlowObfuscation": true,
+    "deadCodeInjection": true,
+    "vmObfuscation": true,
+    "antiDebug": true,
+    "antiTampering": true,
+    "integrityChecks": true,
+    "environmentDetection": true,
+    "timingProtection": true,
+    "obfuscationLevel": 9
+  },
+  "military": {
+    "stringEncryption": true,
+    "variableRenaming": true,
+    "controlFlowObfuscation": true,
+    "deadCodeInjection": true,
+    "vmObfuscation": true,
+    "antiDebug": true,
+    "antiTampering": true,
+    "integrityChecks": true,
+    "environmentDetection": true,
+    "timingProtection": true,
+    "memoryProtection": true,
+    "stackRandomization": true,
+    "opcodeRandomization": true,
+    "obfuscationLevel": 10
+  }
+}
